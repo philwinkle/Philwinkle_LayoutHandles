ba0f9ecc6942e43d514c07503549d0e31949a1b2
Phillip Jackson <philwinkle@gmail.com>
2013-11-16 22:14:17 -0500

    Browser-type layout handle support

--- app/code/community/Philwinkle/LayoutHandles/Model/Observer.php
+++ app/code/community/Philwinkle/LayoutHandles/Model/Observer.php
@@ -1,7 +1,42 @@
 <?php
 class Philwinkle_LayoutHandles_Model_Observer
 {
-    public function addHandle(Varien_Event_Observer $observer)
+    public $handles = array();
+    public $update;
+
+
+    /**
+     * Adding all implemented handles
+     * @todo  Implement as a strategy pattern
+     * @todo  Unify preg_match and stripos positions
+     * @param Varien_Event_Observer $observer
+     */
+    public function addHandles(Varien_Event_Observer $observer)
+    {
+        $this->update = $observer->getEvent()->getLayout()->getUpdate();
+        $this->collectHandles()->processHandles();
+
+        return $this;
+    }
+
+    public function collectHandles()
+    {
+        $this->operatingSystemHandle();
+        $this->browserHandle();
+
+        return $this;
+    }
+
+    public function processHandles()
+    {
+        foreach($this->handles as $handle){
+            $this->update->addHandle($handle);
+        }
+
+        return $this;
+    }
+
+    public function operatingSystemHandle()
     {
         $agent = $_SERVER['HTTP_USER_AGENT'];
 
@@ -15,8 +50,28 @@ class Philwinkle_LayoutHandles_Model_Observer
             $os = 'unknown';
         }
 
-        /* @var $update Mage_Core_Model_Layout_Update */
-        $update = $observer->getEvent()->getLayout()->getUpdate();
-        $update->addHandle('operating_system_' . $os);
+        $this->handles[] = 'operating_system_' . $os;
     }
+
+    public function browserHandle()
+    {
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+
+        if ( stripos($agent, 'Firefox') !== false ) {
+            $agent = 'firefox';
+        } elseif ( stripos($agent, 'MSIE') !== false ) {
+            $agent = 'ie';
+        } elseif ( stripos($agent, 'iPad') !== false ) {
+            $agent = 'ipad';
+        } elseif ( stripos($agent, 'Android') !== false ) {
+            $agent = 'android';
+        } elseif ( stripos($agent, 'Chrome') !== false ) {
+            $agent = 'chrome';
+        } elseif ( stripos($agent, 'Safari') !== false ) {
+            $agent = 'safari';
+        }
+
+        $this->handles[] = 'browser_' . $agent;
+    }
+
 }
